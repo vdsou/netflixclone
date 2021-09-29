06fcca3bf731f3a42980efc2352af347a7fa6597
vdsou <vdsouza@outlook.com>
2021-09-29 18:56:37 -0300

    add getFeaturedItem method

---
+++ src/components/FeaturedItem/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './FeaturedItem.css';
+
+function FeaturedItem() {
+  return (
+    <div>
+      <h1>FeaturedItem</h1>
+    </div>
+  );
+}
+
+export default FeaturedItem;
